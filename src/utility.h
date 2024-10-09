f5f291df65e653ba23c095d3262c7bfa380dc256
Rémi Duvette <remiduvette@gmail.com>
2024-10-09 22:38:58 +0000

    Initial commit of common files

---
+++ src/utility.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// int crop(int min, int max, int value) {
+//     return (value <= min) ? min : ((value >= max) ? max : value);
+// }
+//
+// long long crop(long long min, long long max, long long value) {
+//     return (value <= min) ? min : ((value >= max) ? max : value);
+// }
+
+double crop(double min, double max, double value);
